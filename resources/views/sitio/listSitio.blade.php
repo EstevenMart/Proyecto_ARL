893349af138f64cb5fd87a0bb5fa1f131aaa306f
Yeraldin28 <ypulido46@misena.edu.co>
2021-08-27 02:49:01 -0500

    creacion crud stio otrapersonas

---
+++ resources/views/sitio/listSitio.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+@section('title' , 'Sitio')
+@section('h1' , 'Sitio')
+@section('content')
+
+<a href=" {{route('sitio.formSitio')}} " class="btn btn-primary">Nuevo Sitio</a>
+
+
+@if(Session::has('message'))
+    <p class="text-danger">{{ Session::get('message') }}</p>
+@endif
+@if(Session::has('messa'))
+    <p class="text-primary">{{ Session::get('messa') }}</p>
+@endif
+
+
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>Parte del Cuerpo</th>
+            <th></th>
+        
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ( $listSitio as $sitio )
+            <tr>
+                <td>{{$sitio->denominacionSitio}}</td>
+                
+                <td>
+                  <a href="{{ route('sitio.formSitio', ['id'=> $sitio->id]) }}" class="btn btn-warning">Editar</a> 
+                {{--     <a href="{{ route('sitio.delete' , ['id'=> $sitio->id]) }}" class="btn btn-danger">Borrar</a>  --}}
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\AccidenteController;
 use App\Http\Controllers\AgentAcciController;
+use App\Http\Controllers\SitioController;
+use App\Http\Controllers\OtraPersonaController;
+
+
 
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +34,11 @@ Route::get('/agentAccis' , [AgentAcciController::class , "show"]);
 Route::get('/agentAcci/delete/{id}', [AgentAcciController::class, 'delete'])->name('agentAcci.delete');
 Route::get('/agentAcci/formagentAcci/{id?}', [AgentAcciController::class, 'form'])->name('agentAcci.form');
 Route::post('/agentAcci/save', [AgentAcciController::class, 'save'])->name('agentAcci.save');
+
+Route::get('/sitios' , [SitioController::class , "show"]);
+Route::get('/sitio/formSitio/{id?}', [SitioController::class, 'form'])->name('sitio.formSitio');
+Route::post('/sitio/saveSitio', [SitioController::class, 'save'])->name('sitio.saveSitio');
+
+Route::get('/otrasPersonas' , [OtraPersonaController::class , "show"]);
+Route::get('/otrasPersona/formOtrasPersona/{id?}', [OtraPersonaController::class, 'form'])->name('otraPersona.formOtrasPersona');
+Route::post('/otrasPersona/saveOtrasPersona', [OtraPersonaController::class, 'save'])->name('otraPersona.saveOtraPersona');
